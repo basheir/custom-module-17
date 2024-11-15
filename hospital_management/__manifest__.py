202f8a57ef7012b5d4be02bd6b22c66718698ed8
Bashir Mahad <basheirmahad@gmail.com>
2024-11-15 22:19:01 +0300

    view_ads added with hidding the create button

--- hospital_management/__manifest__.py
+++ hospital_management/__manifest__.py
@@ -10,6 +10,7 @@
 
     "data": [
         'security/ir.model.access.csv',
+        "views/all.doctors.view.xml",
         'views/doctors.view.xml',
         'views/patient_views.xml',
         'views/menu.xml'
