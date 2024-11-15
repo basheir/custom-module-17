0c2f38905824e74c93aff8b6c38a295dc325a286
Bashir Mahad <basheirmahad@gmail.com>
2024-11-15 06:41:33 +0300

    Doctor View and menuitem added

--- hospital_management/__manifest__.py
+++ hospital_management/__manifest__.py
@@ -8,6 +8,7 @@
     "application": True,
     "data": [
         'security/ir.model.access.csv',
+        'views/doctors.view.xml',
         'views/patient_views.xml',
         'views/menu.xml'
     ],
